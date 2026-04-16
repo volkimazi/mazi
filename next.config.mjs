52513a1bdd4225fb465c9ad323d4798ff92b542c
Volki <murshedaliv@gmail.com>
2026-04-17 01:41:34 +0300

    fix: remove next-pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,4 @@
-import withPWA from 'next-pwa';
-
-const pwaConfig = withPWA({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-});
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
@@ -15,4 +7,4 @@ const nextConfig = {
   },
 };
 
-export default pwaConfig(nextConfig);
+export default nextConfig;
